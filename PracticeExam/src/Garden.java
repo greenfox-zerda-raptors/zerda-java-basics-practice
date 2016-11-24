8c75e46fe5d665f7926848691f65513425cd00a1
WarWolf89 <almasi.csaba89@gmail.com>
2016-11-24 08:58:50 +0100

    Garden is finished

---
+++ PracticeExam/src/Garden.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+/**
+ * Created by almasics on 2016.11.24..
+ */
+public class Garden extends ArrayList<Plant> {
+
+    public String water(int amount){
+        int numberOfThirsty = 0;
+        for (Plant p : this){
+            if (p.isThirsty()){
+                numberOfThirsty++;
+            }
+        }
+        int waterPerPlant = (int)((float)amount/numberOfThirsty);
+        for (Plant p : this){
+            if (p.isThirsty()){
+                p.water(waterPerPlant);
+            }
+        }
+        return "Watering with " + amount;
+    }
+
+
+}
